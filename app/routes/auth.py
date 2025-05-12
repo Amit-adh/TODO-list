7615e36129b3b2d5f36eba024475d8abfb159525
unknown <itsman38@gmail.com>
2025-05-12 15:05:30 +0545

    minor changes

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -13,7 +13,6 @@ def login(err=False, err_msg=None):
 @auth.route("/verify_user", methods=["POST"])
 def verify_user():
     from app.models import User
-    print("yes")
     username = request.form['username']
     password = request.form['password']
     user = User.query.filter_by(username=username, password=password).first()
@@ -21,6 +20,5 @@ def verify_user():
         session['user_id'] = user.id
         return redirect(url_for("dashboard"))
     else:
-        print("no")
         return redirect(url_for("auth.login", err=True, err_msg="User does not exist in records."))
     
