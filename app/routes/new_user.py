7986c4278d77f50c3f51ec2b3738e206f0ea2b0b
unknown <itsman38@gmail.com>
2025-05-13 23:03:30 +0545

    incomplete jinja syntax error handling

--- app/routes/new_user.py
+++ app/routes/new_user.py
@@ -1,4 +1,4 @@
-from flask import request, render_template, redirect, url_for, Blueprint
+from flask import request, render_template, redirect, url_for, Blueprint, flash
 import re
 
 new_user = Blueprint("new_user", __name__)
@@ -8,20 +8,27 @@ def new_user_register():
     if request.method == 'POST':
         from app import db
         from app.models import User
-        username = request.form['username']
-        email = request.form['email']
-        password = request.form['password']
-        re_pass = request.form['re-pass']
+        username = request.form.get("username", "")
+        email = request.form.get("email", "")
+        password = request.form.get("password", "")
+        re_pass = request.form.get("repass", "")
         email_pattern = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
         
         if User.query.filter_by(username=username).first():
-            return render_template("register.html", err=True, err_msg="Username already exists")    
+            flash("Username already exists", category="username_error")
+            return render_template("register.html")    
         
         elif not re.match(email_pattern, email):
-            return render_template("register.html", err=True, err_msg="Invalid Email", uname=username)
+            flash("Invalid Email", category="email_error")
+            return render_template("register.html", uname=username)
         
-        elif  password != re_pass:
-            return render_template("register.html", err=True, err_msg="Both passwords must be the same", uname=username, email=email)
+        elif password == "":
+            flash("Password must not be empty", category="empty_password")
+            return render_template("register.html", uname=username, email=email)
+        
+        elif password != re_pass:
+            flash("Both passwords must be the same", category="different_passwords")
+            return render_template("register.html", uname=username, email=email)
         
 
         user = User(username=username, email=email, password=password)
@@ -29,4 +36,4 @@ def new_user_register():
         db.session.commit()
         return redirect(url_for("auth.login"))
     
-    return render_template("register.html", err=False)
+    return render_template("register.html")
